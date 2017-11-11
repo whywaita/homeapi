c719033ab1a14a1e97a7da0d6a41258c5e57174f
whywaita <whywaita@whywrite.it>
2017-11-12 01:37:53 +0900

    switch flag args

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/whywaita/homeapi/cli"
 	"github.com/whywaita/homeapi/server"
 	"go.uber.org/zap"
 )
 
+var (
+	mode = flag.String("m", "server", "change mode, cli / server")
+)
+
 func main() {
 	logger, _ := zap.NewProduction()
+	flag.Parse()
 
-	go server.Run(logger)
+	if *mode == "cli" {
+		cli.Start(logger)
+	} else {
+		server.Run(logger)
+	}
 
-	cli.Start(logger)
 }
