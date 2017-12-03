a2644340d53726eb68c2b1e51923a39abb2d013f
whywaita <whywaita@whywrite.it>
2017-12-03 17:09:53 +0900

    add stage switcher for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,36 @@
 function build() {
   echo "build version ${HASH}"
 
-  go build -o ${BINARY} .
+  GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${BINARY} .
 }
 
 function deploy() {
   echo "deploy version ${HASH}"
 
-  scp ${BINARY} home:/usr/local/bin/yayoi
+  scp ${BINARY} home:yayoi
 }
 
-ACTION="$1"
+function production() {
+  export GOOS="linux"
+  export GOARCH="amd64"
+}
+
+function develop() {
+  export GOOS="darwin"
+  export GOARCH="amd64"
+}
+
+STAGE="$1"
+ACTION="$2"
 
 HASH=`git rev-parse HEAD`
 HASH="${HASH:0:8}"
 
-GOOS="linux"
-GOARCH="amd64"
+case ${STAGE} in
+  "develop" ) develop ;;
+  "production" ) production ;;
+  *) echo "Invalid stage"; exit 2;
+esac
 
 BINARY="yayoi-${GOOS}-${GOARCH}-${HASH}"
 
