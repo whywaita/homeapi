a6faa62495e130e243222e2fed4b2f1a8d37d5d3
whywaita <whywaita@whywrite.it>
2017-12-04 19:20:17 +0900

    display device cards

--- ui/src/components/YayoiWebUI.vue
+++ ui/src/components/YayoiWebUI.vue
@@ -1,22 +1,49 @@
 <template>
-  <div class="hello">
+  <div id="app">
     <h1>Yayoi Web UI</h1>
+    <div class="row" v-for="devices in result">
+      <div class="columns medium-3 medium-6" v-for="device in devices">
+        <div class="card">
+          <div class="card-divider">
+            {{ device.name }}
+          </div>
+          <div class="card-section">
+            {{ device.status }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+const URL_BASE = `${process.env.API_HOST}/api/irkit/`;
+
 export default {
   name: 'YayoiWebUI',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      result: [],
     };
+  },
+  async created() {
+    const url = 'list';
+    try {
+      const resp = await axios.get(URL_BASE + url);
+      this.result = resp.data;
+    } catch (e) {
+      // eslint-disable-next-line
+      console.error(e);
+    }
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 h1, h2 {
   font-weight: normal;
 }
