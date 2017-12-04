cb9af9819770150cf917e5dbd6fd38bcb96c02a1
whywaita <whywaita@whywrite.it>
2017-12-04 19:50:57 +0900

    devide components

---
+++ ui/src/components/DeviceCards.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="row" v-for="devices in result">
+      <div class="columns medium-3 medium-6" v-for="device in devices">
+        <div class="card">
+          <div class="card-divider">
+            {{ device.name }}
+          </div>
+          <div class="card-section">
+            {{ device.status }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const URL_BASE = `${process.env.API_HOST}/api/irkit/`;
+
+export default {
+  data() {
+    return {
+      result: [],
+    };
+  },
+  async created() {
+    const url = 'list';
+    try {
+      const resp = await axios.get(URL_BASE + url);
+      this.result = resp.data;
+    } catch (e) {
+      // eslint-disable-next-line
+      console.error(e);
+    }
+  },
+};
+</script>
--- ui/src/components/YayoiWebUI.vue
+++ ui/src/components/YayoiWebUI.vue
@@ -1,42 +1,17 @@
 <template>
   <div id="app">
     <h1>Yayoi Web UI</h1>
-    <div class="row" v-for="devices in result">
-      <div class="columns medium-3 medium-6" v-for="device in devices">
-        <div class="card">
-          <div class="card-divider">
-            {{ device.name }}
-          </div>
-          <div class="card-section">
-            {{ device.status }}
-          </div>
-        </div>
-      </div>
-    </div>
+    <device-cards></device-cards>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
-const URL_BASE = `${process.env.API_HOST}/api/irkit/`;
+import DeviceCards from './DeviceCards';
 
 export default {
-  name: 'YayoiWebUI',
-  data() {
-    return {
-      result: [],
-    };
-  },
-  async created() {
-    const url = 'list';
-    try {
-      const resp = await axios.get(URL_BASE + url);
-      this.result = resp.data;
-    } catch (e) {
-      // eslint-disable-next-line
-      console.error(e);
-    }
+  name: 'web-ui',
+  components: {
+    DeviceCards,
   },
 };
 </script>
